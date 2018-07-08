1d30a063ba5c56c04a58d61412f86110a27ffb70
chauncyli <chauncyli@tencent.com>
2018-07-08 05:54:07 +0000

    加上代码注释

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,23 +1,20 @@
-Page({
+// 返回识别结果，展示动物详情页面
 
-  /**
-   * 页面的初始数据
-   */
+Page({
+    // 页面的初始数据
   data: {
     img:'./image/1.jpg',
     name:'池鹭',
-    info:'一直内疚卡都吃饭好副本成就撒不错v空间啊是出库成本挂错不该是吃吧看',
+    info:'动物的基本资料',
     website:'wwww.baidu.com',
     history:'查看历史记录',
     show:false
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  
+    // 生命周期函数--监听页面加载，得到数据后进行展示
   onLoad: function(options) {
     console.log(options.result);
-    let res=options.result;
+    let res=options.result; //后台返回的查询结果
     console.log(res.img);
     this.setData({
       /*img:res.img,
@@ -26,28 +23,11 @@ Page({
       website:res.website,
       history:res.history*/
     });
-    // const db = wx.cloud.database({
-    //   env: 'dynamic-3a28b7'
-    // })
-    // db.collection('Birds').doc('swan').get({
-    //    sucess:res=>{
-    //      console.log(res.data);
-    //    },
-    //    fail:err=>{
-    //      console.log(err.errCode,err.errMsg);
-    //    }
-    // });
-  },
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
   },
 
-  /*跳转到历史记录页面 */
+    // 跳转到历史记录页面 
   historyFn() {
-    //页面跳转
+    //跳转页面
     wx.navigateTo({
       url: '../history/history'
     });
