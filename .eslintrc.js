86bc56c7397060ec2068279fc07845d2d92396b3
chauncyli <Dynamic@tencent.com>
2018-07-08 20:57:26 +0800

    es.js

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {},
+    "extends": [
+        "eslint-config-alloy/react",
+    ],
+    "plugins": [],
+    "rules": {},
+    "globals": {
+        "Component": true,
+        "wx": true,
+        "Page": true,
+        "App": true
+    }
+};
