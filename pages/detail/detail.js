9d3f25f60e4d9ed8f0ab0c38c76e0662622e205f
chauncyli <chauncyli@tencent.com>
2018-07-08 12:22:16 +0000

    jsDOC

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,12 +1,19 @@
-// 查看动物详情页面
-
+/**
+ * 查看动物详情页面
+ */
 Page({
-  // 页面的初始数据
+  /**
+   * 页面的初始数据
+   * 
+  */
   data: {
     history : '查看历史记录'
   },
 
-  // 生命周期函数--监听页面加载
+  /**
+   * 生命周期函数--监听页面加载
+   * @options 页面跳转传递的参数
+  */
   onLoad: function(options) {
 //  做一个判断，结果识别成功和失败呈现不同的效果
     if (options.result == 'null' || options.result == null) {  //识别未知生物
@@ -18,7 +25,8 @@ Page({
         ename : ''
       });
     } 
-    else {  //识别生物成功
+    else {  
+        //识别生物成功
       this.setData({
         img : options.img,
         name : options.name,
@@ -29,7 +37,10 @@ Page({
     }
   },
 
-  // 跳转到历史记录页面
+  /**
+   * 跳转到历史记录页面
+   * 
+  */
   historyFn() {
     //页面跳转
     wx.navigateTo({
